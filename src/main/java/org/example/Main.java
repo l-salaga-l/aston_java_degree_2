c1e23efb5eecdbd6bc1df2f6a3ccce977a8f2cfe
R2okie <damir_ni@mail.ru>
2024-10-27 20:41:55 +0300

    feat: Add usage example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.homework.hw1.CustomArrayListImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CustomArrayListImpl<Integer> list = new CustomArrayListImpl<>();
+        list.add(0, 5);
+        list.add(1, 2);
+        list.add(2, 4);
+        list.add(3, 1);
+        list.add(4, 3);
+
+        System.out.println(list);
+        list.sort(Integer::compareTo);
+        System.out.println(list);
     }
 }
